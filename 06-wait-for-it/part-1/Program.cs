39b821bda30485ab4e0887d0b3a0c90ab791cc7c
Joseph Dvorak <dvorakjt@gmail.com>
2023-12-06 01:39:49 -0500

    added solutions for day 6

---
+++ 06-wait-for-it/part-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+string inputFilePath = "./input.txt";
+
+if(!File.Exists(inputFilePath))
+{
+  throw new FileNotFoundException("Input file input.txt not found in current directory.");
+}
+
+string[] input = File.ReadAllLines(inputFilePath);
+
+Func<int, int, int> countWaysToWin = (raceTime, recordDistance) =>
+{
+  int chargingTime = 0;
+  int possibleWaysToWin = 0;
+
+  for( ; raceTime >= 0; raceTime--, chargingTime++)
+  {
+    if(raceTime * chargingTime > recordDistance) possibleWaysToWin++;
+  }
+
+  return possibleWaysToWin;
+};
+
+List<int> raceTimes = new(Regex.Matches(input[0], @"\d+").Select(match => int.Parse(match.Value)));
+List<int> raceRecords = new(Regex.Matches(input[1], @"\d+").Select(match => int.Parse(match.Value)));
+
+int marginForError = 1;
+
+for(int i = 0; i < raceTimes.Count; i++)
+{
+  marginForError *= countWaysToWin(raceTimes[i], raceRecords[i]);
+}
+
+Console.WriteLine($"The margin for error is {marginForError}");
---
+++ 06-wait-for-it/part-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+string inputFilePath = "./input.txt";
+
+if(!File.Exists(inputFilePath))
+{
+  throw new FileNotFoundException("Input file input.txt not found in current directory.");
+}
+
+string[] input = File.ReadAllLines(inputFilePath);
+
+Func<string, long> getTime = s =>
+{
+  return long.Parse(string.Join("", Regex.Matches(s, @"\d+").Select(match => match.Value)));
+};
+
+Func<long, long, long> countWaysToWin = (raceTime, recordDistance) =>
+{
+  long chargingTime = 0;
+  long possibleWaysToWin = 0;
+
+  for( ; raceTime >= 0; raceTime--, chargingTime++)
+  {
+    if(raceTime * chargingTime > recordDistance) possibleWaysToWin++;
+  }
+
+  return possibleWaysToWin;
+};
+
+long raceTime = getTime(input[0]);
+long raceRecord = getTime(input[1]);
+
+long waysToWin = countWaysToWin(raceTime, raceRecord);
+
+Console.WriteLine($"The number of ways to win is {waysToWin}");
